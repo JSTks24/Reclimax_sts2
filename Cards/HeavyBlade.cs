5cd5ce7e7d5c18c83a1876e1cc59515e3d2fbe8f
JSTks24 <2677299914@qq.com>
2026-03-26 19:51:13 +0800

    Fix the damage of Heavy Blade

--- Cards/HeavyBlade.cs
+++ Cards/HeavyBlade.cs
@@ -1,11 +1,11 @@
 ﻿using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
 using MegaCrit.Sts2.Core.Models.Powers;
-using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Luminous.Cards;
@@ -13,10 +13,15 @@ public class HeavyBlade : CardModel {
     public override CardPoolModel Pool => ModelDb.CardPool<IroncladCardPool>();
     public HeavyBlade() : base(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) { }
     protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(14, ValueProp.Move), new EnergyVar(3)];
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource) {
+        if (Owner.Creature != dealer) {
+            return 0m;
+        }
+        StrengthPower? strength = Owner.Creature.GetPower<StrengthPower>();
+        return DynamicVars.Energy.BaseValue * (strength?.Amount ?? 0);
+    }
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
-        StrengthPower? strength = this.Owner.Creature.GetPower<StrengthPower>();
-        decimal damage = this.DynamicVars.Damage.BaseValue + this.DynamicVars.Energy.BaseValue * (strength?.Amount - 1 ?? 0);
-        await DamageCmd.Attack(damage).Targeting(cardPlay.Target!).FromCard((CardModel)this).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+        await DamageCmd.Attack(this.DynamicVars.Damage.BaseValue).Targeting(cardPlay.Target!).FromCard((CardModel)this).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
     }
     protected override void OnUpgrade() => this.DynamicVars.Energy.UpgradeValueBy(2);
 }
