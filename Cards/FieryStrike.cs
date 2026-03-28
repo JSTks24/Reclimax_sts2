947a42be8e288bf490b78fb167ea3b057bee2b66
JSTks24 <2677299914@qq.com>
2026-03-28 18:38:08 +0800

    Finish the first version of the mod.

---
+++ Cards/FieryStrike.cs
@@ -0,0 +1,19 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Luminous.Cards;
+public sealed class FieryStrike : CardModel {
+    public override CardPoolModel Pool => ModelDb.CardPool<IroncladCardPool>();
+    public FieryStrike() : base(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy) { }
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(12, ValueProp.Move)];
+    public override int MaxUpgradeLevel => int.MaxValue;
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        await DamageCmd.Attack(this.DynamicVars.Damage.BaseValue).Targeting(cardPlay.Target!).FromCard((CardModel)this).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+    }
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(4 + CurrentUpgradeLevel);
+}
--- Hook/PoolHook.cs
+++ Hook/PoolHook.cs
@@ -1,37 +1,43 @@
 ﻿using HarmonyLib;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.RelicPools;
 using System.Reflection;
 
 namespace Luminous.Hook;
 
 [HarmonyPatch(typeof(IroncladCardPool), "GenerateAllCards")]
 class IroncladPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupPool<IroncladCardPool>(ref __result);
+    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<IroncladCardPool>(ref __result);
 }
 
 [HarmonyPatch(typeof(DefectCardPool), "GenerateAllCards")]
 class DefectPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupPool<DefectCardPool>(ref __result);
+    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<DefectCardPool>(ref __result);
 }
 
 [HarmonyPatch(typeof(SilentCardPool), "GenerateAllCards")]
 class SilentPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupPool<SilentCardPool>(ref __result);
+    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<SilentCardPool>(ref __result);
 }
 
 [HarmonyPatch(typeof(NecrobinderCardPool), "GenerateAllCards")]
 class NecrobinderCardPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupPool<NecrobinderCardPool>(ref __result);
+    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<NecrobinderCardPool>(ref __result);
 }
 
 [HarmonyPatch(typeof(RegentCardPool), "GenerateAllCards")]
 class RegentCardPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupPool<RegentCardPool>(ref __result);
+    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<RegentCardPool>(ref __result);
+}
+
+[HarmonyPatch(typeof(SharedRelicPool), "GenerateAllRelics")]
+class SharedRelicPoolHook {
+    static void Postfix(ref IEnumerable<RelicModel> __result) => Util.SetupRelicPool<SharedRelicPool>(ref __result);
 }
 
 static class Util {
-    public static void SetupPool<T>(ref IEnumerable<CardModel> result) where T : CardPoolModel {
+    public static void SetupCardPool<T>(ref IEnumerable<CardModel> result) where T : CardPoolModel {
         List<CardModel> cards = [.. result];
 
         Assembly currentAssembly = Assembly.GetExecutingAssembly();
@@ -48,5 +54,23 @@ static class Util {
         }
 
         result = [.. cards];
+    }
+    public static void SetupRelicPool<T>(ref IEnumerable<RelicModel> result) where T : RelicPoolModel {
+        List<RelicModel> relics = [.. result];
+
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        var metrelicinfo = typeof(ModelDb).GetMethod("Relic");
+        var subTypes = currentAssembly.GetTypes()
+                .Where(t => t.IsClass
+                         && !t.IsAbstract
+                         && t.IsSubclassOf(typeof(RelicModel)))
+                .ToList();
+        foreach (Type relic in subTypes) {
+            var inst = metrelicinfo!.MakeGenericMethod(relic).Invoke(null, null);
+            if (relic.GetProperty("Pool")!.GetValue(inst) is T)
+                relics.Add((RelicModel)inst!);
+        }
+
+        result = [.. relics];
     }
 }
