abfd2de46b79eda540d0e195694703f05d5e7ce8
JSTks24 <2677299914@qq.com>
2026-03-28 22:08:28 +0800

    Fix the reflection logic.

--- Hook/PoolHook.cs
+++ Hook/PoolHook.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
 using MegaCrit.Sts2.Core.Models.RelicPools;
@@ -6,71 +7,59 @@ using System.Reflection;
 
 namespace Luminous.Hook;
 
-[HarmonyPatch(typeof(IroncladCardPool), "GenerateAllCards")]
-class IroncladPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<IroncladCardPool>(ref __result);
-}
-
-[HarmonyPatch(typeof(DefectCardPool), "GenerateAllCards")]
-class DefectPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<DefectCardPool>(ref __result);
-}
-
-[HarmonyPatch(typeof(SilentCardPool), "GenerateAllCards")]
-class SilentPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<SilentCardPool>(ref __result);
-}
-
-[HarmonyPatch(typeof(NecrobinderCardPool), "GenerateAllCards")]
-class NecrobinderCardPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<NecrobinderCardPool>(ref __result);
-}
-
-[HarmonyPatch(typeof(RegentCardPool), "GenerateAllCards")]
-class RegentCardPoolHook {
-    static void Postfix(ref IEnumerable<CardModel> __result) => Util.SetupCardPool<RegentCardPool>(ref __result);
-}
-
-[HarmonyPatch(typeof(SharedRelicPool), "GenerateAllRelics")]
-class SharedRelicPoolHook {
-    static void Postfix(ref IEnumerable<RelicModel> __result) => Util.SetupRelicPool<SharedRelicPool>(ref __result);
-}
-
-static class Util {
-    public static void SetupCardPool<T>(ref IEnumerable<CardModel> result) where T : CardPoolModel {
-        List<CardModel> cards = [.. result];
-
-        Assembly currentAssembly = Assembly.GetExecutingAssembly();
-        var metcardinfo = typeof(ModelDb).GetMethod("Card");
-        var subTypes = currentAssembly.GetTypes()
-                .Where(t => t.IsClass
-                         && !t.IsAbstract
-                         && t.IsSubclassOf(typeof(CardModel)))
-                .ToList();
-        foreach (Type card in subTypes) {
-            var inst = metcardinfo!.MakeGenericMethod(card).Invoke(null, null);
-            if (card.GetProperty("Pool")!.GetValue(inst) is T)
-                cards.Add((CardModel)inst!);
-        }
-
-        result = [.. cards];
+[HarmonyPatch]
+class CardPoolHook {
+    static IEnumerable<MethodBase> TargetMethods() {
+        yield return AccessTools.Method(typeof(IroncladCardPool), "GenerateAllCards");
+        yield return AccessTools.Method(typeof(DefectCardPool), "GenerateAllCards");
+        yield return AccessTools.Method(typeof(SilentCardPool), "GenerateAllCards");
+        yield return AccessTools.Method(typeof(NecrobinderCardPool), "GenerateAllCards");
+        yield return AccessTools.Method(typeof(RegentCardPool), "GenerateAllCards");
     }
-    public static void SetupRelicPool<T>(ref IEnumerable<RelicModel> result) where T : RelicPoolModel {
-        List<RelicModel> relics = [.. result];
-
-        Assembly currentAssembly = Assembly.GetExecutingAssembly();
-        var metrelicinfo = typeof(ModelDb).GetMethod("Relic");
-        var subTypes = currentAssembly.GetTypes()
-                .Where(t => t.IsClass
-                         && !t.IsAbstract
-                         && t.IsSubclassOf(typeof(RelicModel)))
-                .ToList();
-        foreach (Type relic in subTypes) {
-            var inst = metrelicinfo!.MakeGenericMethod(relic).Invoke(null, null);
-            if (relic.GetProperty("Pool")!.GetValue(inst) is T)
-                relics.Add((RelicModel)inst!);
+    static void Postfix(object __instance, ref CardModel[] __result) {
+        if (ModCache.CardCache.TryGetValue(__instance.GetType(), out var cachedCards)) {
+            __result = [.. __result, .. cachedCards];
         }
+    }
+}
 
-        result = [.. relics];
+[HarmonyPatch]
+class RelicPoolHook {
+    static IEnumerable<MethodBase> TargetMethods() {
+        yield return AccessTools.Method(typeof(SharedRelicPool), "GenerateAllRelics");
+    }
+    static void Postfix(object __instance, ref IEnumerable<RelicModel> __result) {
+        if (ModCache.RelicCache.TryGetValue(__instance.GetType(), out var cachedRelics)) {
+            __result = [.. __result, .. cachedRelics];
+        }
+    }
+}
+
+static class ModCache {
+    internal static readonly Dictionary<Type, List<CardModel>> CardCache = [];
+    internal static readonly Dictionary<Type, List<RelicModel>> RelicCache = [];
+    static ModCache() {
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        var subTypes = currentAssembly.GetTypes();
+        var classType = typeof(ModelDb);
+        DealHook(classType.GetMethod("Card")!, subTypes, ref CardCache);
+        DealHook(classType.GetMethod("Relic")!, subTypes, ref RelicCache);
+    }
+    private static void DealHook<T>(MethodInfo methodInfo, Type[] subTypes, ref Dictionary<Type, List<T>> caches){
+        var types = subTypes.Where(t => t.IsClass
+                         && !t.IsAbstract
+                         && t.IsSubclassOf(typeof(T)))
+                .ToList();
+        foreach (Type type in types) {
+            var inst = methodInfo!.MakeGenericMethod(type).Invoke(null, null);
+            var poolType = type.GetProperty("Pool")?.GetValue(inst)?.GetType();
+            if (poolType != null) {
+                if (!caches.TryGetValue(poolType, out var list)) {
+                    list = [];
+                    caches[poolType] = list;
+                }
+                list.Add((T)inst!);
+            }
+        }
     }
 }
